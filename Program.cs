46453c1c653eeea555f0dc7dcf60a6cd4b7747ab
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-31 22:47:47 +0530

    Added check email program

--- Program.cs
+++ Program.cs
@@ -17,6 +17,12 @@ namespace UserRegistrationRegex
             Console.WriteLine("Please Enter your last name");
             var check2 = lastName.ValidateFirstName(Console.ReadLine());
             Console.WriteLine(check2);
+
+            //UC3
+            UC3CheckEmail email = new UC3CheckEmail();
+            Console.WriteLine("Please Enter your email to check");
+            var check3 = email.ValidateEMail(Console.ReadLine());
+            Console.WriteLine(check3);
         }
     }
 }
