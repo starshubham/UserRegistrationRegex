590a22fe1ffb9a8d88ab25cf7bfa46305469b9e4
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-31 23:04:44 +0530

    Added password3 program

---
+++ UC7Password3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+Rule3 – Should have at least 1 numeric number in the password
+- NOTE – All rules must be passed
+*/
+
+namespace UserRegistrationRegex
+{
+    public class UC7Password3
+    {
+        public static string REGEX_Password3 = "^[A-Z]{1}[A-Z a-z]{7,}[0-9]{1,}$";  //Shubhamsth23
+
+        public bool ValidatePassword3(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password3);
+        }
+    }
+}
