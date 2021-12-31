3c9d9db0fc5b6867bd832e7845fc2997e2e270a4
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-31 22:31:11 +0530

    Added Program.cs file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            UC1FirstName firstName = new UC1FirstName(); ;
+            Console.WriteLine("Please Enter your first name");
+            var check = firstName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check);
+           
+        }
+    }
+}
