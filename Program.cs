99c90a326ed885d50c3675c759137531a301df11
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-31 22:40:17 +0530

    Added last name program

--- Program.cs
+++ Program.cs
@@ -11,7 +11,12 @@ namespace UserRegistrationRegex
             Console.WriteLine("Please Enter your first name");
             var check = firstName.ValidateFirstName(Console.ReadLine());
             Console.WriteLine(check);
-           
+
+            //UC2
+            UC2LastName lastName = new UC2LastName();
+            Console.WriteLine("Please Enter your last name");
+            var check2 = lastName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check2);
         }
     }
 }
---
+++ UC2LastName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/*
+As a User need to enter a valid Last Name
+- Last name starts with Cap and has minimum 3 characters
+*/
+
+
+namespace UserRegistrationRegex
+{
+    public class UC2LastName
+    {
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string lName)
+        {
+            return Regex.IsMatch(lName, LAST_NAME_REGEX);
+        }
+    }
+}
